50bc13009a69ca5cbe8b7ef731c499d9bc17faee
mcwillbmac <mcwillbmac@gmail.com>
2021-02-01 02:00:14 -0600

    added new methods to DaoOpps class

--- src/main/java/project/dao/DaoOpps.java
+++ src/main/java/project/dao/DaoOpps.java
@@ -1,33 +1,50 @@
 package project.dao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Scanner;
 import project.customers.*;
 import project.accounts.*;
+import project.connectionUtil.Connections;
 
 public class DaoOpps {
 	
-	public String register() {
-		Scanner sc = new Scanner(System.in);
-		Customer cus = new Customer();
-		System.out.println("Enter your First Name.");
-		cus.firstName = sc.nextLine();
-		cus.setFirstName(cus.firstName);
-		System.out.println("Enter your Last Name.");
-		cus.lastName = sc.nextLine();
-		cus.setLastName(cus.lastName);
-		System.out.println("Enter your address.");
-		cus.address = sc.nextLine();
-		cus.setAddress(cus.address);
-		System.out.println("Create a username");
-		cus.userName = sc.nextLine();
-		cus.setUserName(cus.userName);
-		System.out.println("Create a password");
-		cus.passWord = sc.nextLine();
-		cus.setPassWord(cus.passWord);
-		String accountInfo = new String();
-		accountInfo = cus.firstName + " "+ cus.lastName + " " + cus.address + " " + cus.userName;
-		sc.close();
-		return accountInfo;
+	public int insert(Customer u) {
+
+		try {
+			
+			Connection conn = Connections.getConnection();
+
+			
+			String columns = "first_name, last_name ,user_name ,password ,address";
+			String sql = "INSERT INTO users (" + columns + ") VALUES (?, ?, ?, ?, ?)";
+			
+
+			// Step 3a: Obtain Statement Object
+			// PreparedStatement is a sub interface of Statement that provides extra
+			// security to prevent SQL injection.
+			PreparedStatement stmt = conn.prepareStatement(sql);
+
+			// Step 3b: Inject values to replace all the ? marks
+			stmt.setString(1, u.getUserName()); // replace 1st ? mark with u.getUsername()
+			stmt.setString(2, u.getPassWord()); // replace 2nd ? mark with u.getPassword()
+			stmt.setString(3, u.getFirstName()); // replace 3rd ? mark with u.getFirstName()
+			stmt.setString(4, u.getLastName());
+			stmt.setString(5, u.getAddress());
+			 // this is returning the int value for the Role id of the Role object
+
+			// Step 4: Execute the statement;
+			return stmt.executeUpdate(); // this will return the number of statements executed (1)
+
+		} catch (SQLException e) {
+			// Step 5: Perform any exception handling in an appropriate means
+			e.printStackTrace();
+		}
+
+		// otherwise we return 0 if we cannot insert a row into the database
+		return 0;
 	}
+
 	
 
 }
---
+++ src/main/java/project/service/userService.java
@@ -0,0 +1,33 @@
+package project.service;
+
+import java.util.Scanner;
+
+import project.customers.Customer;
+
+public class userService {
+
+	public String register() {
+		Scanner sc = new Scanner(System.in);
+		Customer cus = new Customer();
+		System.out.println("Enter your First Name.");
+		cus.firstName = sc.nextLine();
+		cus.setFirstName(cus.firstName);
+		System.out.println("Enter your Last Name.");
+		cus.lastName = sc.nextLine();
+		cus.setLastName(cus.lastName);
+		System.out.println("Enter your address.");
+		cus.address = sc.nextLine();
+		cus.setAddress(cus.address);
+		System.out.println("Create a username");
+		cus.userName = sc.nextLine();
+		cus.setUserName(cus.userName);
+		System.out.println("Create a password");
+		cus.passWord = sc.nextLine();
+		cus.setPassWord(cus.passWord);
+		String accountInfo = new String();
+		accountInfo = cus.firstName + " "+ cus.lastName + " " + cus.address + " " + cus.userName;
+		sc.close();
+		return accountInfo;
+	}
+	
+}
